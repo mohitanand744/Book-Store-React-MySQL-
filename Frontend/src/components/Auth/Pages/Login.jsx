175e3d45f1df2e19255f2e98ed6f961aade6f76c
mohitanand744 <jhamohit744@gmail.com>
2025-01-11 19:49:53 +0530

    working on login page

--- Frontend/src/components/Auth/Pages/Login.jsx
+++ Frontend/src/components/Auth/Pages/Login.jsx
@@ -1,7 +1,17 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Login = () => {
-  return <center>Login</center>;
+  return (
+    <center className="flex flex-col items-center justify-center h-screen gap-4">
+      <h1>Working on Login page</h1>
+      <Link to={"/bookstore"}>
+        <button className="px-4 py-2 text-white bg-gray-600 rounded-2xl">
+          Go to Website
+        </button>
+      </Link>
+    </center>
+  );
 };
 
 export default Login;
